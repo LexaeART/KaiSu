2f7227442f71fc2d66dfd4cc4cab5dd39c64d4ef
LexaeART <42806298+LexaeART@users.noreply.github.com>
2018-08-29 14:36:50 +0200

    Bit bit

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-08-28 11:04:27 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-08-28T11:04:27+00:00	INFO ::1	joomlafailure	Vous devez indiquer un mot de passe!
+2018-08-28T11:33:22+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
